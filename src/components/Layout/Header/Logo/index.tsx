a20906715cc961b6d1f5d94529aee365babced6c
YRACHEK101 <r.yahia.dev@gmail.com>
2025-02-26 23:21:58 +0000

    fix  logo  text in dark

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Logo: React.FC = () => {
-  return <Link href="/" className="text-4xl font-semibold">Liagen</Link>;
-};
+  return (
+    <Link href="/" className="text-4xl font-semibold text-black">
+      Liagen
+    </Link>
+  );
+}
 
 export default Logo;
